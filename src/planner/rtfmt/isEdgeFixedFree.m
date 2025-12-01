794426e893a35bfa70527819c6e5402ebc037113
Edison <e.dimar1999@gmail.com>
2025-12-01 11:00:21 -0500

    Feat: add Dubins support to edge check
    Updated isEdgeFixedFree to handle Dubins connections when 'conn' is passed. This allows checking dynamic and static obstacles for both straight-line and Dubins paths.

--- src/planner/rtfmt/isEdgeFixedFree.m
+++ src/planner/rtfmt/isEdgeFixedFree.m
@@ -1,11 +1,14 @@
 function tf = isEdgeFixedFree(i, j, S)
 
-    % Static obstacle check                             
-    res = S.map.Resolution;
-    [pts,~,~] = interpLine(res, S.V(i,:), S.V(j,:), [0; 0]);
+    % Static obstacle check 
+    if ~isempty(S.conn)
+        [pts,~,~] = interpDubins(S.conn, S.V(i,:), S.V(j,:), [0; 0]);
+    else                            
+        res = S.map.Resolution;
+        [pts,~,~] = interpLine(res, S.V(i,:), S.V(j,:), [0; 0]);
+    end
     occ = checkOccupancy(S.map, pts(:,1:3));
-    % tf  = all(occ < map.FreeThreshold);   
-    tf_static  = all(occ < 0.2);
+    tf_static  = all(occ < S.map.FreeThreshold);
 
     if ~tf_static
         tf = false; return;
@@ -20,32 +23,22 @@ function tf = isEdgeFixedFree(i, j, S)
         tf = false; return;
     end
 
-    pi = S.V(i,1:3); pj = S.V(j,1:3);
-
-    for k = 1:size(S.dynObs,1)
-        c = S.dynObs(k,1:3);
-        R = S.safeRadiusDObstacle;
-
-        if segmentIntersectsSphere3D(pi, pj, c, R)
-            tf = false; return;
-        end
+    if segmentIntersectsSphere3D(pts, S.dynObs, S.safeRadiusDObstacle)
+        tf = false; 
+        return;
     end
+
+    % pi = S.V(i,1:3); pj = S.V(j,1:3);
+    % 
+    % for k = 1:size(S.dynObs,1)
+    %     c = S.dynObs(k,1:3);
+    %     R = S.safeRadiusDObstacle;
+    % 
+    %     if segmentIntersectsSphere3D(pi, pj, c, R)
+    %         tf = false; return;
+    %     end
+    % end
 
     tf = true;
 end
 
-function tf = segmentIntersectsSphere3D(a, b, c, R)
-% True if the segment ab touches the sphere (c, R)
-    ab = b - a;
-    ac = c - a;
-    L2 = dot(ab, ab);
-    if L2 == 0
-        d2 = dot(ac, ac);
-        tf = d2 <= R*R;
-        return;
-    end
-    t  = max(0, min(1, dot(ac, ab) / L2));
-    q  = a + t * ab;
-    d2 = sum((q - c).^2);
-    tf = d2 <= R*R;
-end
